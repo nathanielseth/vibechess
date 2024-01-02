006af5a9b5e592a6227261696c0606d012d45ecd
nathzu <nathanielseth.dev@gmail.com>
2024-01-02 14:53:47 +0800

    organized directory structure

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+	font-size: 30px;
+	font-weight: 600;
+`;
+
+function App() {
+	return (
+		<div>
+			<H1>VibeChess</H1>
+		</div>
+	);
+}
+
+console.log("test");
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	root: "./client",
+	publicDir: "public",
+	plugins: [react(), eslint()],
+	server: {
+		port: 3000,
+	},
+});
