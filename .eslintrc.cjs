455408c40cb59fad5ceb0f9224f63d9bdde5c731
nathzu <nathanielseth.dev@gmail.com>
2025-07-13 23:09:18 +0800

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true, node: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: { react: { version: "18.2" } },
+	plugins: ["react-refresh", "react", "react-hooks"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"no-mixed-spaces-and-tabs": 0,
+		"react/jsx-uses-react": 1,
+		"react/jsx-uses-vars": 1,
+		"react/react-in-jsx-scope": 1,
+	},
+};
